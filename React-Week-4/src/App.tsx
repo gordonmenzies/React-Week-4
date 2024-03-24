e219f8c3a0cf7b7dfc5a76b9d1f1240076fc71ce
gordonmenzies <heymish96@gmail.com>
2024-03-24 20:38:49 +0000

    search name and search role functionality added

--- React-Week-4/src/App.tsx
+++ React-Week-4/src/App.tsx
@@ -5,48 +5,55 @@ import "./App.scss";
 import { useState } from "react";
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
+  //const [inputValue, setInputValue] = useState("");
   const [teamArray, setTeamArray] = useState(team);
-  // save the original array so that the values that are entered are rememebered
+  //const [selectValue, setSelectValue] = useState("");
 
-  // setTeamArray(team);
+  const searchEmployee = (event: React.FormEvent<HTMLInputElement>) => {
+    const value = event.currentTarget.value.toLowerCase();
+    const searchedGroup = teamArray.filter((member) => {
+      return member.name.toLowerCase().includes(value);
+    });
+    setTeamArray(searchedGroup);
+    if (value === "") {
+      setTeamArray(team);
+    }
+  };
+
+  const searchRole = (event: React.FormEvent<HTMLSelectElement>) => {
+    let searchedGroup = [...team];
+    const value = event.currentTarget.value.toLowerCase();
+    searchedGroup = team.filter((member) => {
+      return member.role.toLowerCase().includes(value);
+    });
+    setTeamArray(searchedGroup);
+    if (value === "") {
+      setTeamArray(team);
+    }
+  };
 
   const employeeList = teamArray.map((member) => {
     return <Employee key={member.id} name={member.name} role={member.role} />;
   });
 
-  // how to update to the new inputValue rather than the previous inputValue
-
-  const searchEmployee = (event: React.FormEvent<HTMLInputElement>) => {
-    console.log("event.target", event.currentTarget.value.toLowerCase());
-    setInputValue(event.currentTarget.value.toLowerCase());
-    console.log("inputValue", inputValue);
-    const searchedGroup = teamArray.filter((member) => {
-      return member.name.toLowerCase().includes(inputValue);
-    });
-    setTeamArray(searchedGroup);
-    if (inputValue === "") {
-      setTeamArray(team);
-    }
-  };
-
   return (
-    <div className="tiketTracker__container">
+    <div className="ticketTracker__container">
       <h1> Ticket Tracker</h1>
       <input
         type="text"
         className="ticketTracker__employeeSearchInput"
         onChange={searchEmployee}
       />
-      <button
-        className="ticketTrackee__employeeListReset"
-        onClick={() => {
-          setTeamArray(team);
-          setInputValue("");
-        }}
-      >
-        reset list
-      </button>
+      <select className="ticketTracker__roleSelect" onChange={searchRole}>
+        <option value="">--Please choose an option--</option>
+        <option value="Software Developer">Software Developer</option>
+        <option value="Junior Software Developer">
+          Juniour Software Developer
+        </option>
+        <option value="Project Manager">Project Manager</option>
+        <option value="Tester">Tester</option>
+        <option value="Hard Man">Hard Man</option>
+      </select>
       <div className="employeeList__container">{employeeList}</div>
     </div>
   );
