dc17d94fcc286b3a687d4e373bc62bb52b1c8988
gordonmenzies <heymish96@gmail.com>
2024-03-25 14:33:26 +0000

    finished with basic mobile styling and two extensions

--- React-Week-4/src/App.tsx
+++ React-Week-4/src/App.tsx
@@ -5,10 +5,9 @@ import "./App.scss";
 import { useState } from "react";
 
 function App() {
-  //const [inputValue, setInputValue] = useState("");
   const [teamArray, setTeamArray] = useState(team);
-  //const [selectValue, setSelectValue] = useState("");
 
+  // set state array with only elements that fit the search terms
   const searchEmployee = (event: React.FormEvent<HTMLInputElement>) => {
     const value = event.currentTarget.value.toLowerCase();
     const searchedGroup = teamArray.filter((member) => {
@@ -20,6 +19,7 @@ function App() {
     }
   };
 
+  // set state array with only elements that fit the search terms
   const searchRole = (event: React.FormEvent<HTMLSelectElement>) => {
     let searchedGroup = [...team];
     const value = event.currentTarget.value.toLowerCase();
@@ -32,28 +32,32 @@ function App() {
     }
   };
 
+  // map over list of employees
   const employeeList = teamArray.map((member) => {
     return <Employee key={member.id} name={member.name} role={member.role} />;
   });
 
   return (
     <div className="ticketTracker__container">
-      <h1> Ticket Tracker</h1>
-      <input
-        type="text"
-        className="ticketTracker__employeeSearchInput"
-        onChange={searchEmployee}
-      />
-      <select className="ticketTracker__roleSelect" onChange={searchRole}>
-        <option value="">--Please choose an option--</option>
-        <option value="Software Developer">Software Developer</option>
-        <option value="Junior Software Developer">
-          Juniour Software Developer
-        </option>
-        <option value="Project Manager">Project Manager</option>
-        <option value="Tester">Tester</option>
-        <option value="Hard Man">Hard Man</option>
-      </select>
+      <h1>Ticket Tracker</h1>
+      <div className="ticketTracker__searchContainer">
+        <label>Search By Name</label>
+        <input
+          type="text"
+          className="ticketTracker__employeeSearchInput"
+          onChange={searchEmployee}
+        />
+        <select className="ticketTracker__roleSelect" onChange={searchRole}>
+          <option value="">--Search By Role--</option>
+          <option value="Software Developer">Software Developer</option>
+          <option value="Junior Software Developer">
+            Juniour Software Developer
+          </option>
+          <option value="Project Manager">Project Manager</option>
+          <option value="Tester">Tester</option>
+          <option value="Hard Man">Hard Man</option>
+        </select>
+      </div>
       <div className="employeeList__container">{employeeList}</div>
     </div>
   );
