817f057cf5625e420429236f488676180663c4cd
gordonmenzies <heymish96@gmail.com>
2024-03-26 08:33:11 +0000

    each employee has it's own state

--- React-Week-4/src/Components/Counter/Counter.tsx
+++ React-Week-4/src/Components/Counter/Counter.tsx
@@ -1,25 +1,16 @@
 import "./Counter.scss";
-import { useState } from "react";
 
-const Counter = () => {
-  const [counter, setCounter] = useState(0);
+type CountProps = {
+  count: number;
+  increment: () => void;
+  decrement: () => void;
+};
 
-  const increment = () => {
-    setCounter(counter + 1);
-  };
-
-  const decrement = () => {
-    if (counter === 0) {
-      return;
-    } else {
-      setCounter(counter - 1);
-    }
-  };
-
+const Counter = ({ count, increment, decrement }: CountProps) => {
   return (
     <div className="counter__container">
       <h3 onClick={decrement}>-</h3>
-      <h3>{counter}</h3>
+      <h3>{count}</h3>
       <h3 onClick={increment}>+</h3>
     </div>
   );
--- React-Week-4/src/Components/Employee/Employee.tsx
+++ React-Week-4/src/Components/Employee/Employee.tsx
@@ -1,5 +1,6 @@
 import "./Employee.scss";
 import Counter from "../Counter/Counter";
+import { useState } from "react";
 
 type EmployeeProps = {
   name: string;
@@ -7,11 +8,25 @@ type EmployeeProps = {
 };
 
 const Employee = ({ name, role }: EmployeeProps) => {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrement = () => {
+    if (counter === 0) {
+      return;
+    } else {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
     <div className="employee__container">
       <p>{name}</p>
       <p>{role}</p>
-      <Counter />
+      <Counter count={counter} increment={increment} decrement={decrement} />
     </div>
   );
 };
